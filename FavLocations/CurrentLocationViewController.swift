38b4892bf79d077032148710a7c7cf7d6345f44f
Timofei Shchepkin <dgrz5.0@gmail.com>
2020-01-12 19:52:55 -0500

    get current location

--- FavLocations/CurrentLocationViewController.swift
+++ FavLocations/CurrentLocationViewController.swift
@@ -7,16 +7,54 @@
 //
 
 import UIKit
+import CoreLocation
 
-class CurrentLocationViewController: UIViewController {
+class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
+    //MARK: Outlets
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longtitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    let locationManager = CLLocationManager()
     
+    //MARK: Actions
     
+    @IBAction func getLocation() {
+        locationManager.delegate = self
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    //MARK: Delegate
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Did fail erroneously \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last
+        print("didUpdateLocation \(newLocation)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print(status)
+        // ?
+        if status == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+    }
+    
 
 }
 
