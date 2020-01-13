7a59456a51a84378ef0af5e96aaf18a704d2fcc6
Timofei Shchepkin <dgrz5.0@gmail.com>
2020-01-12 19:04:10 -0500

    Initial Commit

---
+++ FavLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FavLocations
+//
+//  Created by Tim Fosteman on 2020-01-12.
+//  Copyright © 2020 Fosteman's Software Solutions LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
